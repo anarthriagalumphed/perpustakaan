acec6e2976e771731a32dbf3cce749a4a8dea202
anarthriagalumphed <galih22.mp@gmail.com>
2023-03-28 20:46:28 +0700

    konfigurasi

---
+++ database/migrations/2023_03_28_132442_create_books_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->strin('judul');
+            $table->string('pengarang');
+            $table->string('penerbit');
+            $table->integer('tahun_terbit');
+            $table->integer('jumlah_halaman');
+            $table->text('sinopsis');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
