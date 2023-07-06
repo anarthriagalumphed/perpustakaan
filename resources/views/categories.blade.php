784c35dad6b5db2273332ca9493fbdb9941e7012
anarthriagalumphed <galih22.mp@gmail.com>
2023-07-06 19:32:40 +0700

    bug di categories

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -10,7 +10,7 @@
             {{ session('status') }}
         </div>
     @endif
-    <div class="card">
+    <div class="card card-success">
         <div class="card-header">
 
             <h3 class="card-title">@yield('title')</h3>
@@ -20,7 +20,7 @@
             <table id="example1" class="table table-bordered table-striped">
 
 
-                <div class="form-inline">
+                {{-- <div class="form-inline">
                     <div class="input-group" data-widget="sidebar-search">
                         <input class="form-control form-control-sidebar" type="search" placeholder="Search"
                             aria-label="Search">
@@ -30,7 +30,7 @@
                             </button>
                         </div>
                     </div>
-                </div>
+                </div> --}}
                 <br>
                 <a href="add_category" class="btn btn-primary btn-sm mb-2" style="margin-right: 10px;"><i
                         class="fas fa-plus"></i>
@@ -40,109 +40,35 @@
                         class="fas fa-history"></i>
                     View Deleted
                     Data</a>
-                <br>@extends('layouts.mainlayout')
+                <br>
+                <tr>
+                    <th>No.</th>
+                    <th>Name</th>
+                    <th>Action</th>
+                </tr>
 
-            @section('title', 'Categories')
+                <!-- ini perlu diganti data => isi -->
 
+                <!-- ini perlu diganti -->
+                <tbody>
+                    @foreach ($category as $item)
+                        <tr>
+                            <td>{{ $loop->iteration }}</td>
+                            <td>{{ $item->name }}</td>
+                            <td>
+                                <a href="edit_category/{{ $item->slug }}" class="btn btn-warning btn-sm"><i
+                                        class="fas fa-edit"></i></a>
+                                <a href="delete_category/{{ $item->slug }}" class="btn btn-danger btn-sm"><i
+                                        class="fas fa-trash"></i></a>
 
-            @section('content')
+                            </td>
+                        </tr>
+                    @endforeach
 
-                @if (session('status'))
-                    <div class="alert alert-success">
-                        {{ session('status') }}
-                    </div>
-                @endif
-                <div class="card card-success">
-                    <div class="card-header">
+                </tbody>
 
-                        <h3 class="card-title">@yield('title')</h3>
-                    </div>
-                    <!-- /.card-header -->
-                    <div class="card-body">
-                        <table id="example1" class="table table-bordered table-striped">
-
-
-                            {{-- <div class="form-inline">
-                                    <div class="input-group" data-widget="sidebar-search">
-                                        <input class="form-control form-control-sidebar" type="search" placeholder="Search"
-                                            aria-label="Search">
-                                        <div class="input-group-append">
-                                            <button class="btn btn-sidebar">
-                                                <i class="fas fa-search fa-fw"></i>
-                                            </button>
-                                        </div>
-                                    </div>
-                                </div> --}}
-                            <br>
-                            <a href="add_category" class="btn btn-primary btn-sm mb-2" style="margin-right: 10px;"><i
-                                    class="fas fa-plus"></i>
-                                Tambah
-                                Category</a>
-                            <a href="deleted_category" class="btn btn-warning btn-sm mb-2" style="margin-right: 10px;"><i
-                                    class="fas fa-history"></i>
-                                View Deleted
-                                Data</a>
-                            <br>
-                            <tr>
-                                <th>No.</th>
-                                <th>Name</th>
-                                <th>Action</th>
-                            </tr>
-
-                            <!-- ini perlu diganti data => isi -->
-
-                            <!-- ini perlu diganti -->
-                            <tbody>
-                                @foreach ($category as $item)
-                                    <tr>
-                                        <td>{{ $loop->iteration }}</td>
-                                        <td>{{ $item->name }}</td>
-                                        <td>
-                                            <a href="edit_category/{{ $item->slug }}" class="btn btn-warning btn-sm"><i
-                                                    class="fas fa-edit"></i></a>
-                                            <a href="delete_category/{{ $item->slug }}" class="btn btn-danger btn-sm"><i
-                                                    class="fas fa-trash"></i></a>
-
-                                        </td>
-                                    </tr>
-                                @endforeach
-
-                            </tbody>
-
-                        </table>
-                    </div>
-                </div>
-
-            @endsection
-
-            <tr>
-                <th>No.</th>
-                <th>Name</th>
-
-            </tr>
-
-            <!-- ini perlu diganti data => isi -->
-
-            <!-- ini perlu diganti -->
-            <tbody>
-                @foreach ($category as $item)
-                    <tr>
-                        <td>{{ $loop->iteration }}</td>
-                        <td>{{ $item->name }}</td>
-                        <td>
-                            <a href="edit_category/{{ $item->slug }}" class="btn btn-warning btn-sm"><i
-                                    class="fas fa-edit"></i></a>
-                            <a href="delete_category/{{ $item->slug }}" class="btn btn-danger btn-sm"><i
-                                    class="fas fa-trash"></i></a>
-
-                        </td>
-                    </tr>
-                @endforeach
-
-            </tbody>
-
-        </table>
+            </table>
+        </div>
     </div>
-</div>
 
 @endsection
